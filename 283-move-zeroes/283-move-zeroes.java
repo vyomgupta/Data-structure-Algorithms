e87b86ceee088f318228f4878684fb7e3b399643
vyomgupta <vyomguptarock@gmail.com>
2022-09-22 18:08:50 +0530

    Time: 2 ms (84.63%), Space: 54.8 MB (52.51%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,10 +1,10 @@
 class Solution {
     
-    public void swap(int nums[], int first, int second){
-        int temp = nums[first];
-        nums[first] = nums[second];
-        nums[second] = temp;
-    }
+    // public void swap(int nums[], int first, int second){
+    //     int temp = nums[first];
+    //     nums[first] = nums[second];
+    //     nums[second] = temp;
+    // }
     public void moveZeroes(int[] nums) {
         
         int s  = 0;
