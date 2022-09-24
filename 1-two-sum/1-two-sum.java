814dea5246b10826cf46233752aeee65073c5a9d
vyomgupta <vyomguptarock@gmail.com>
2022-09-24 11:05:25 +0530

    Time: 6 ms (64.40%), Space: 45.7 MB (33.14%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -6,15 +6,13 @@ class Solution {
         int ans[] = new int[2];
         for(int i=0 ; i<nums.length ; i++){
             if(map.containsKey(target - nums[i])){
-                ans[0]=i;
-                ans[1]=map.get(target - nums[i]);
-                return ans;
+                return new int[]{i, map.get(target - nums[i])};
             }
             else{
                 map.put(nums[i],i);
             }
         }
-        return ans;
+        return new int[2];
         
         
     }
