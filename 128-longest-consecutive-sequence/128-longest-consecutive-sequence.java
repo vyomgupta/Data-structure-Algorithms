42a347e33470be73e089c6d0e43707a7ddd5ef3d
vyomgupta <vyomguptarock@gmail.com>
2022-09-25 10:56:58 +0530

    Time: 754 ms (16.13%), Space: 152.5 MB (20.26%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,39 +1,33 @@
 class Solution {
-    public int longestConsecutive(int[] nums) {
-        
-        if(nums.length==0) return 0;
-        Arrays.sort(nums);
-        int max = 1;
-        int count = 1;
-        for(int i=0; i<nums.length-1 ; i++){
-            System.out.print(nums[i]+" ");
-            if(nums[i+1] == nums[i] ){
-                continue;
+    public int longestConsecutive(int[] arr) {
+        HashMap<Integer, Boolean> map = new HashMap<>();
+      for (int val : arr) {
+         map.put(val, true);
+      }
+
+      for (int val : arr) {
+         if (map.containsKey(val - 1)) {
+            map.put(val, false);
+         }
+      }
+
+      int msp = 0;
+      int ml = 0;
+      for (int val : arr) {
+         if(map.get(val) == true){
+            int tsp = val;
+            int tl = 1;
+
+            while(map.containsKey(tsp + tl)){
+               tl++;
             }
-            if(nums[i+1] - nums[i] == 1){
-                count++;
+
+            if(tl > ml){
+               ml = tl;
+               msp = tsp;
             }
-            else{
-                count = 1;
-            }
-            max = Math.max(max,count);
-        }
-//         HashMap<Integer,Boolean> map = new HashMap<>();
-        
-//         for(int val : nums){
-//             map.put(val , true);
-//         }
-        
-//         for(int val: map.keySet()){
-//             int count = 0;
-//             while(map.containsKey(val)){
-//                 count++;
-//                 val++;
-//             }
-//              max = Math.max(count,max);
-//         }
-        
-        return max;
-        
+         }
+      }
+        return ml;
     }
 }
