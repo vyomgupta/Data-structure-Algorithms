40f79a9bb0a69e74ba0375ce6429865d2bf7ae45
vyomgupta <vyomguptarock@gmail.com>
2022-09-25 11:51:41 +0530

    Time: 2 ms (52.00%), Space: 53.7 MB (76.19%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,14 +1,19 @@
 class Solution {
     
-    public void mark0(int [][] matrix, int r, int c){
+    public void mark0r(int [][] matrix, int r){
+    
+        for(int i=0;i<matrix[0].length;i++){
+            matrix[r][i]=0;
+        }
+        
+    }
+    
+    public void mark0c(int [][] matrix, int c){
     
         for(int i=0;i<matrix.length;i++){
             matrix[i][c]=0;
         }
         
-        for(int i=0;i<matrix[0].length;i++){
-            matrix[r][i]=0;
-        }
     }
     
     public void setZeroes(int[][] matrix) {
@@ -16,23 +21,28 @@ class Solution {
         int m = matrix[0].length;
         
         boolean mat[][] = new boolean[n][m];
+        boolean row[] = new boolean[n];
+        boolean col[] = new boolean[m];
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
-                    mat[i][j] = true;
+                    row[i] = true;
+                    col[j] = true;
                 }
             }
         }
         
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(mat[i][j]==true){
-                    mark0(matrix,i,j);
+                if(row[i]){
+                    mark0r(matrix,i);
                 }
-            }
         }
-        
+        for(int i=0;i<m;i++){
+                if(col[i]){
+                    mark0c(matrix,i);
+                }
+        }
         
         
     }
