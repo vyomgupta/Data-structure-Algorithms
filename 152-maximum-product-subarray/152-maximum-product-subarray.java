26feb7f6a4c6232f81b802be7c1fb462eb143d53
vyomgupta <vyomguptarock@gmail.com>
2022-09-27 21:24:23 +0530

    Time: 1 ms (97.59%), Space: 44.3 MB (81.10%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -3,49 +3,27 @@ class Solution {
         
         int n = nums.length;
         int sum = 1;
-        int sum2 = 1;
         int max = Integer.MIN_VALUE;
         
         for(int i = 0 ; i < n ; i++){
             int val = nums[i];
+            sum*=val;
+            max = Math.max(sum,max);  
             if(val==0){
-                max = Math.max(max,0);
-                sum=1;
-                sum2=1;
+               sum=1;
             }
-            else if(val<0){
-                sum2*=val;
-                sum=1;
-                max =Math.max(sum2,max);
-            }
-            else{
-                System.out.println("ho");
-               sum*=val; 
-               sum2*=val;
-                max = Math.max(sum,Math.max(sum2,max));
-            }
-            
+                      
         }
-        sum = 1; sum2=1;
+        
+        sum = 1;
+        
         for(int i = n-1 ; i >= 0 ; i--){
-            int val = nums[i];
+           int val = nums[i];
+            sum*=val;
+            max = Math.max(sum,max);  
             if(val==0){
-                max = Math.max(max,0);
-                sum=1;
-                sum2=1;
+               sum=1;
             }
-            else if(val<0){
-                sum2*=val;
-                sum=1;
-                max =Math.max(sum2,max);
-            }
-            else{
-                System.out.println("ho");
-               sum*=val; 
-               sum2*=val;
-                max = Math.max(sum,Math.max(sum2,max));
-            }
-            
         }
         return max;
         
