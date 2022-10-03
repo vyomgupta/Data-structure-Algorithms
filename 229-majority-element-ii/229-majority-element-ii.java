15c5114825e5873fe34a62e87d94e8b6ecac2334
vyomgupta <vyomguptarock@gmail.com>
2022-10-03 22:15:07 +0530

    Time: 3 ms (87.31%), Space: 49.3 MB (76.47%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,51 +1,43 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         
+        
         List<Integer> ans = new ArrayList<>();
         
-        int fcandidate=Integer.MIN_VALUE;
-        int scandidate=Integer.MAX_VALUE;
-        int fcsum=0;
-        int scsum=0;
+        int poss1=Integer.MIN_VALUE;;
+        int poss2=Integer.MAX_VALUE;;
+        int count1=0;
+        int count2=0;
         
         
-        for(int i=0 ; i<nums.length ; i++){
+        for(int i=0;i<nums.length;i++){
             int val = nums[i];
-            if(val == fcandidate){
-                fcsum++;
+             if(poss1 == val){
+                count1++;
             }
-            else if(val == scandidate){
-                scsum++;
+            else if(poss2 == val){
+                count2++;
             }
-            else if(fcsum == 0){
-                fcandidate = val;
-                fcsum = 1;
+            else if(count1 ==0){
+                poss1= val;
+                count1=1;
             }
-            else if(scsum == 0){
-                scandidate = val;
-                scsum = 1;
+            
+            else if(count2 == 0){
+                poss2 = val;
+                count2 = 1;
             }
             else{
-                fcsum--;
-                scsum--;
+                count1--; count2--;
             }
         }
+        int c1 = 0; int c2 = 0;
+        for(int i=0;i<nums.length;i++) if(nums[i]== poss1) c1++;
+        for(int i=0;i<nums.length;i++) if(nums[i]== poss2) c2++;
         
-        int count1=0; int count2=0;
-        
-        for(int i=0 ; i<nums.length ; i++){
-            int val = nums[i];
-            if(val==fcandidate) count1++;
-            if(val==scandidate) count2++;
-        }
-        
-        if(count1> nums.length/3){
-            ans.add(fcandidate);
-        }
-        if(count2> nums.length/3){
-            ans.add(scandidate);
-        }
-        
+        if(c1>nums.length/3) ans.add(poss1);
+        if(c2>nums.length/3) ans.add(poss2);
+
         return ans;
     }
 }
