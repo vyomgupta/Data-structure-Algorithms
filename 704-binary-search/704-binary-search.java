cf9250c7749c1c3c64ab8780751de1218d167b9b
vyomgupta <vyomguptarock@gmail.com>
2022-10-03 22:53:25 +0530

    Time: 0 ms (100.00%), Space: 54 MB (70.24%) - LeetHub

---
+++ 704-binary-search/704-binary-search.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int s = 0;
+        int e = n - 1;
+        
+        while(s <= e){
+            int mid = (s + e) / 2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                s = mid + 1;
+            }
+            else{
+                e = mid - 1;
+            }
+        }
+        return -1;
+        
+    }
+}
