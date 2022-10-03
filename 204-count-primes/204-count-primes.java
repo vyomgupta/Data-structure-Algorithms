1c7776be8c61c76f451b80f45a6129d5297a0209
vyomgupta <vyomguptarock@gmail.com>
2022-10-03 19:38:31 +0530

    Time: 156 ms (70.41%), Space: 68.3 MB (58.80%) - LeetHub

--- 204-count-primes/204-count-primes.java
+++ 204-count-primes/204-count-primes.java
@@ -1,19 +1,23 @@
 class Solution {
     public int countPrimes(int n) {
         
-        boolean size[] = new boolean[n+1];
+        
+        boolean primes[] = new boolean[n+1];
+        
         
         for(int i=2;i*i<=n;i++){
-            if(size[i]==false){
-                for(int j=2;j*i<n;j++){
-                    size[i*j]=true;
+            if(primes[i] == false){
+                for(int j=2;i*j<n;j++){
+                    primes[j*i] = true;
                 }
             }
         }
+        
         int count = 0;
         
         for(int i=2;i<n;i++){
-            if(size[i]==false){
+            if(primes[i] == false){
+               // System.out.print(i+" ")
                 count++;
             }
         }
