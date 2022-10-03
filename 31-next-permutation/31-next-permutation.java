0eae0cd5002601b22e7ee83aff4d5ec6e4c5e0b6
vyomgupta <vyomguptarock@gmail.com>
2022-10-03 21:40:11 +0530

    Time: 1 ms (91.95%), Space: 43.3 MB (79.59%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,49 +1,45 @@
 class Solution {
     
-    public void swap(int nums[], int n1, int n2){
-        int temp = nums[n1];
-        nums[n1] = nums[n2];
-        nums[n2] = temp;
+    public void swap(int nums[], int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
     
     public void reverse(int nums[], int s, int e){
         while(s<e){
             int temp = nums[s];
-            nums[s]  = nums[e];
+            nums[s] = nums[e];
             nums[e] = temp;
             s++; e--;
         }
     }
-    
     public void nextPermutation(int[] nums) {
         
         int n = nums.length;
-        int sidx = -1;
-        int gidx = -1;
-        
-        for(int i = n-2 ; i>=0 ; i--){
-            if(nums[i]<nums[i+1]){
-                sidx = i;
+        int maxSorted = -1;
+        for(int i = n - 1 ; i>0 ; i--){
+            if(nums[i-1] < nums[i]){
+                maxSorted = i-1;
                 break;
             }
         }
         
-        if(sidx == -1){
-            reverse(nums,0,n-1);
+        if(maxSorted == -1){
+           reverse(nums, 0, n-1);
         }
         else{
-            for(int j = n-1 ; j > sidx ; j--){
-                int diff = nums[j] - nums[sidx];
-                if(nums[sidx] < nums[j]){
-                    gidx = j;
+            int gMaxSorted = -1;
+            for(int i = n - 1 ; i>=0 ; i--){
+                if(nums[maxSorted] < nums[i]){
+                    gMaxSorted = i;
                     break;
                 }
             }
-            swap(nums,sidx,gidx);
-            
-            reverse(nums,sidx+1,n-1);
-        }
         
+            swap(nums,maxSorted,gMaxSorted);
+            reverse(nums,maxSorted+1,n-1);
+        }
         
         
         
