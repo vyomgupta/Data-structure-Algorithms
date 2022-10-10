1df49bbb4c369d49820495eba0fe78a4e2443a03
Vyom Gupta <vyomguptarock@gmail.com>
2022-10-10 16:25:19 +0530

    Time: 0 ms (100.00%), Space: 60.4 MB (55.39%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
@@ -1,52 +1,13 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        
-        int n = nums.length/2;
-    //    System.out.println(n+1);
-        
-        
-        int s = 0; int e = nums.length - 1 ;
-        
-        if(nums.length ==1){
-            return nums[0];
+        int left = 0, right = nums.length-1;
+        while(left < right){
+            int mid = (left + right)/2;
+            if( (mid % 2 == 0 && nums[mid] == nums[mid +1]) || (mid %2 == 1 && nums[mid] == nums[mid - 1]) )
+                left = mid + 1;
+            else
+                right = mid;
         }
-        while(s <= e){
-            int mid = s +(e - s) /2;
-            System.out.println(mid+" " +nums[mid]);
-            if(mid == 0  && nums[mid+1]!=nums[mid]){
-                return nums[mid];
-            }
-            else if(mid == nums.length - 1 && nums[mid-1]!=nums[mid]){
-                return nums[mid];
-            }
-            else if(mid>0 && mid < nums.length -1 && (nums[mid] != nums[mid+1] && nums[mid] != nums[mid-1]) ){
-                
-                return nums[mid];
-            }
-            else if(mid < nums.length -1 && nums[mid] == nums[mid+1]){
-                if((mid+1)%2==0){
-                    e = mid - 1;
-                }
-                else{
-                    s = mid + 1;
-                }
-            }
-            else if(mid>0 && nums[mid] == nums[mid-1]){
-                if(mid%2==0){
-                    e = mid - 1;
-                }
-                else{
-                    s = mid + 1;
-                }
-            }
-            else{
-                s = mid + 1;
-            }
-            
-            
-        }
-        
-        return -1;
-        
-    }
+        return nums[left];
+    }   
 }
