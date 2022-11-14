882783fa708152c005197c23b9f42d7165cc9527
Vyom Gupta <vyomguptarock@gmail.com>
2022-11-14 17:38:54 +0530

    Time: 56 ms (7.73%), Space: 45.5 MB (22.96%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int no = x;
+        int newno=0;
+        int ld=0;
+        
+        while(no!=0){
+            ld = no % 10;
+            no = no / 10;
+            int oldno = newno;
+            newno = (newno * 10) + ld;
+            // if((newno - 1)/10  != oldno){
+            //     return false;
+            // }
+            
+        }
+        
+        if(ld<0){
+            return false;
+        }
+        
+        System.out.println(newno);
+        return x == newno;
+    }
+}
