dd4ea386a6726bfeb60092fc3f4f4b95d66b2c80
Vyom Gupta <vyomguptarock@gmail.com>
2023-02-17 18:30:14 +0530

    Added solution - LeetHub

---
+++ Leaders in an array - GFG/leaders-in-an-array.java
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Array {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter ot = new PrintWriter(System.out);
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		
+		while(t-->0){
+		    
+		    //input size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //inserting elements in the array
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    StringBuffer str = new StringBuffer();
+		    ArrayList<Integer> res = new ArrayList<Integer>();
+		  
+		    //calling leaders() function
+		    res = obj.leaders(arr, n);
+		    
+
+		    for(int i=0; i<res.size(); i++){
+		        ot.print(res.get(i)+" ");
+		    }
+		    
+		    ot.println();
+		}
+		ot.close();
+		
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution{
+    //Function to find the leaders in the array.
+    static ArrayList<Integer> leaders(int arr[], int n){
+        // Your code here
+        ArrayList<Integer> ans = new ArrayList<>();
+        int max = 0;
+        
+        for(int i = n - 1 ; i>=0 ; i --){
+            int val = arr[i];
+            if(val>=max){
+                ans.add(val);
+            }
+            max = Math.max(max,val);
+        }
+        
+        int s =0;
+        int e = ans.size() - 1;
+        
+        while(s <= e){
+            int temp = ans.get(s);
+            ans.set(s,ans.get(e));
+            ans.set(e,temp);
+            s++;
+            e--;
+        }
+        return ans;
+    }
+}
