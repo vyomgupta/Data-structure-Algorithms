bbffa35a80e030d9a29debcf768126f5b5d97180
Vyom Gupta <vyomguptarock@gmail.com>
2023-02-17 17:59:21 +0530

    Time: 10 ms (5.80%), Space: 50.8 MB (99.93%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,15 +3,31 @@ class Solution {
         
         int sum = 0;
         int max = Integer.MIN_VALUE;
-        
+        int s = 0;
+        int e= 0;
+        boolean first = false;
         for(int i = 0 ; i < nums.length ; i ++){
             int val = nums[i];
             sum += val;
-            max = Math.max(max,sum);
+            if(sum > max){
+                if(first == true){
+                    e = i;
+                }
+                else{
+                    first = true;
+                    s = i;
+                    e = i;
+                }
+                max = sum;
+                
+            }
+            
             if(sum < 0){
                 sum = 0;
+                first = false;
             }
         }
+        System.out.println(s+" "+e);
         return max;
         
     }
