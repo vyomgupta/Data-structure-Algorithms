6675390a64682dbc2f809c5c91f98576c3e2195f
Vyom Gupta <vyomguptarock@gmail.com>
2023-02-28 23:33:05 +0530

    Time: 21 ms (62.79%), Space: 46.8 MB (87.96%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
@@ -1,26 +1,33 @@
 class Solution {
     
-    public boolean isPossible(int nums[], int treshold, int mid){
-        int count = 0;
-        for(int i=0;i<nums.length;i++){
-            int val = nums[i];
-            count+=val/mid;
-            if(val%mid!=0) count++;
-        }
-        return count<=treshold?true:false;
-    }
     
+    public boolean isValid(int nums[], int mid, int threshold){
+        int count = 0;
+        for(int i = 0 ; i < nums.length ;  i ++){
+            int val = nums[i];
+            
+            int cal = val / mid;
+            count+=cal;
+            int rem = val%  mid;
+            if(rem!=0){
+                count++;
+            }
+            
+        }
+        
+        return count <= threshold;
+    }
     public int smallestDivisor(int[] nums, int threshold) {
-        
         int s = 1;
-        int e = 1000000009;
+        int e = Integer.MAX_VALUE - 2;
         int ans = -1;
-        
-        while(s<=e){
-            int mid = s+(e-s)/2;
-            if(isPossible(nums,threshold,mid)){
-                e = mid - 1;
+
+        while(s <= e){
+            int mid = s + (e - s)/2;
+            
+            if(isValid(nums,  mid, threshold)){
                 ans = mid;
+                e = mid - 1;
             }
             else{
                 s = mid + 1;
@@ -28,6 +35,9 @@ class Solution {
         }
         
         return ans;
+        
+        
+        
         
     }
 }
