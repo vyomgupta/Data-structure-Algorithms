cac01e22d109d3199f0df521262ddaf6e7586fc5
Vyom Gupta <vyomguptarock@gmail.com>
2023-04-07 22:49:12 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (76.45%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -3,22 +3,20 @@ class Solution {
         
         int s = 0;
         int e = nums.length - 1;
-        int ans = -1;
         
         while(s <= e){
-            int mid = s + (e - s) / 2;
-            if(nums[mid] < target){
-                s = mid + 1;
-                ans = mid;
-            }
-            else if(nums[mid] > target){
-                e = mid - 1;
-            }
-            else{
+            int mid = s + (e - s )/2;
+            if(nums[mid] == target){
                 return mid;
             }
+            else if(nums[mid] < target){
+                s = mid + 1;
+            }
+            else{
+                e = mid - 1;
+            }
         }
-        return ans + 1;
+        return s;
         
     }
 }
