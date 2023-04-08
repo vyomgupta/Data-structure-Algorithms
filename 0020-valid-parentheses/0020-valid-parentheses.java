89f8f1efe676add8bce3605506363e50da2579ce
Vyom Gupta <vyomguptarock@gmail.com>
2023-04-08 10:19:23 +0530

    Time: 2 ms (77.68%), Space: 40.8 MB (31.95%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        Stack<Character> st = new Stack<>();
+        
+        for(int i = 0 ; i < s.length() ; i ++){
+            char ch = s.charAt(i);
+            if(ch == '(' || ch =='[' || ch == '{'){
+                st.push(ch);
+            }
+            else if(ch == ')'){
+                if(st.size() > 0 && st.peek() == '(') st.pop();
+                else return false;
+            }
+            else if(ch == ']'){
+                if(st.size() > 0 && st.peek() == '[') st.pop();
+                else return false;
+            }
+            else if(ch == '}'){
+                if(st.size() > 0 && st.peek() == '{') st.pop();
+                else return false;
+            }
+            
+        }
+        return st.size() == 0 ? true : false;
+        
+    }
+}
